afaaa2c3b1e07ffa5a17967c978d5e4bbde5d733
leodaiub <leodaiub@gmail.com>
2019-06-12 22:50:19 -0300

    finished back-end, starting now to develop the front-end

---
+++ back-end/src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const multer = require('multer');
+const likeController = require('./controllers/likeController');
+const postController = require('./controllers/postController');
+const uploadConfig = require('./config/upload');
+
+const routes = new express.Router();
+
+const upload = multer(uploadConfig);
+
+routes.post('/posts', upload.single('image'), postController.store );
+routes.get('/posts', postController.index);
+routes.post('/posts/:id/like', likeController.store);
+
+module.exports = routes;
