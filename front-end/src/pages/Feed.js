47e918554684b91e386fff5ecc31e70cb82621d3
leodaiub <leodaiub@gmail.com>
2019-06-14 00:01:30 -0300

    finished react.js front-end

--- front-end/src/pages/Feed.js
+++ front-end/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component }  from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 
@@ -16,16 +17,38 @@ class Feed extends Component {
     };
 
     async componentDidMount() {
+        this.registerToSocket();
+
         const response = await api.get('posts');
 
         this.setState({feed: response.data});
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3030');
+
+        socket.on('post', newPost => {
+            this.setState( { feed: [newPost, ... this.state.feed]} );
+        });
+
+        socket.on('like', likedPost => {
+            this.setState({
+                feed: this.state.feed.map(post => 
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        })
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`)
     }
 
     render(){
         return(
             <section id="post-list">
               {this.state.feed.map(post => (
-                  <article>
+                  <article key={post._id}>
                   <header>
                       <div class="user-info">
                           <span>{post.author}</span>
@@ -37,12 +60,14 @@ class Feed extends Component {
 
                   <footer>
                       <div className="actions">
+                    <button type="button" onClick={ () => this.handleLike(post._id)}>
                       <img src={like} alt="Like" />
+                    </button>
                       <img src={comment} alt="Comment" />
                       <img src={send} alt="Send" />
                       </div>
 
-                      <strong>{post.likes}Curtidas</strong>
+                      <strong>{post.likes} Curtidas</strong>
                       <p>
                               {post.description}
                           <span>{post.hashtags}</span>
